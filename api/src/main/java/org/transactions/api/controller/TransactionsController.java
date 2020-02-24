697ba8fc0b6dd77b53a0d3dec31e4966575ea5ab
jgirard <girard.julien@gmail.com>
2020-02-24 22:23:42 +0100

    Init Rest Controller

--- api/src/main/java/org/transactions/api/controller/TransactionsController.java
+++ api/src/main/java/org/transactions/api/controller/TransactionsController.java
@@ -1,4 +1,4 @@
-package org.transactions.server.api;
+package org.transactions.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- api/src/test/java/org/transactions/api/ServerApplicationTests.java
+++ api/src/test/java/org/transactions/api/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package org.transactions.server;
+package org.transactions.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
