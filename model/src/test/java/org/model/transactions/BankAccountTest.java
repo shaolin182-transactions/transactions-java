d29b0e70766ebcd81a43596990b8f48b2213aff8
jgirard <girard.julien@gmail.com>
2020-08-11 12:10:53 +0200

    Fix Code coverage

--- model/src/test/java/org/model/transactions/BankAccountTest.java
+++ model/src/test/java/org/model/transactions/BankAccountTest.java
@@ -1,9 +1,12 @@
 package org.model.transactions;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.model.transactions.BankAccount.BankAccountBuilder;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
@@ -29,7 +32,7 @@ class BankAccountTest {
     @ParameterizedTest(name="Validation test on bank account - run #{index} with [{arguments}]")
     @MethodSource("getBankAccountDataSet")
     void validBankAccount(String category, Integer id, String label, Integer expectedError) {
-        BankAccount bankAccount = new BankAccount.BankAccountBuilder()
+        BankAccount bankAccount = new BankAccountBuilder()
                 .withLabel(label)
                 .withId(id)
                 .withCategory(category)
@@ -51,5 +54,24 @@ class BankAccountTest {
           Arguments.of(random(100, true, true), 100, "A bank Account Label", 1),
           Arguments.of("A bank Account Category", 100, random(100, true, true), 1)
         );
+    }
+
+    @Test
+    void testEqualsBankAccountObject() {
+        BankAccount bkAcc1 = new BankAccountBuilder()
+                .withCategory("someCat").withLabel("label").withId(1)
+                .build();
+
+        BankAccount bkAcc2 = new BankAccountBuilder()
+                .withCategory("someCat").withLabel("label").withId(1)
+                .build();
+
+        BankAccount bkAcc3 = new BankAccountBuilder()
+                .withCategory("someCat").withLabel("label").withId(2)
+                .build();
+
+        Assertions.assertEquals(bkAcc1, bkAcc2, "Category with same properties values should be equals");
+        Assertions.assertEquals(bkAcc1, bkAcc1, "Same object should be equals");
+        Assertions.assertNotEquals(bkAcc1, bkAcc3, "Category with different properties values should be equals");
     }
 }
