c03a6aeb5086d95a27e1dc692d680c62d55ee048
jgirard <girard.julien@gmail.com>
2020-09-08 13:58:14 +0200

    Configure App for several env

--- api/src/main/java/org/transactions/api/ServerApplication.java
+++ api/src/main/java/org/transactions/api/ServerApplication.java
@@ -2,10 +2,13 @@ package org.transactions.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages="org.transactions")
 @EnableMongoRepositories("org.transactions.persistence.repositories")
+@PropertySource("classpath:application.properties")
+@PropertySource({"classpath:application-${envTarget:dev}.properties"})
 public class ServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
