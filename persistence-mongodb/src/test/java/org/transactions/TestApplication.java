76a0698e3062eae67ee5942f490ed174c5168e56
jgirard <girard.julien@gmail.com>
2021-02-23 16:01:13 +0100

    Replace property files by YAML files

--- persistence-mongodb/src/test/java/org/transactions/TestApplication.java
+++ persistence-mongodb/src/test/java/org/transactions/TestApplication.java
@@ -1,7 +1,9 @@
 package org.transactions;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.yaml")
 public class TestApplication {
 }
