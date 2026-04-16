e35543ffec0c1b97d63a99153ec0dc0f3429d89a
Julien Girard <girard.julien@gmail.com>
2026-04-16 21:22:24 +0200

    feat(postgres): Integrate Postgresql

--- persistence-postgresql/src/main/java/org/transactions/persistence/pg/CommonPgDatasource.java
+++ persistence-postgresql/src/main/java/org/transactions/persistence/pg/CommonPgDatasource.java
@@ -1,8 +1,11 @@
 package org.transactions.persistence.pg;
 
+import org.apache.commons.lang3.StringUtils;
 import org.model.transactions.BankAccount;
 import org.model.transactions.TransactionCategory;
 import org.model.transactions.TransactionCategoryType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
@@ -20,6 +23,8 @@ import java.util.stream.StreamSupport;
 @Component("posgresCommonDatasource")
 @Profile("postgresql")
 public class CommonPgDatasource implements ICommonDataDatasource {
+
+    private static final Logger log = LoggerFactory.getLogger(CommonPgDatasource.class);
 
     private List<TransactionCategory> categories;
 
@@ -94,11 +99,30 @@ public class CommonPgDatasource implements ICommonDataDatasource {
     @Override
     public void refresh() {
         categories = StreamSupport.stream(catRepository.findAll().spliterator(), false)
-                .map(entity -> new TransactionCategory.TransactionCategoryBuilder().withCategory(entity.getCategory()).withId(entity.getId()).withType(TransactionCategoryType.valueOf(entity.getType())).withLabel(entity.getLabel()).build())
+                .map(entity -> buildCategory(entity))
                 .toList();
 
         bankAccounts = StreamSupport.stream(bkRepository.findAll().spliterator(), false)
                 .map(entity -> new BankAccount.BankAccountBuilder().withCategory(entity.getCategory()).withId(entity.getId()).withLabel(entity.getLabel()).build())
                 .toList();;
+    }
+
+    private static TransactionCategory buildCategory(CategoryEntity entity) {
+        var builder = new TransactionCategory.TransactionCategoryBuilder();
+
+        builder
+                .withCategory(entity.getCategory())
+                .withId(entity.getId())
+                .withLabel(entity.getLabel());
+
+        if (StringUtils.isNotEmpty(entity.getType())){
+            try {
+                builder.withType(TransactionCategoryType.valueOf(entity.getType()));
+            } catch (IllegalArgumentException e) {
+                log.atWarn().setCause(e).log("Invalid category type {} for category id {}, category will be ignored", entity.getType(), entity.getId());
+            }
+        }
+
+        return builder.build();
     }
 }
