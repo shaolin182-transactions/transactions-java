346d68d236f9dc4585999635dce727374b6d69bd
Julien Girard <girard.julien@gmail.com>
2026-07-20 14:03:03 +0200

    feat: set FAIL_ON_UNKNOWN_PROPERTIES to false

--- service/src/main/java/org/transactions/configuration/JacksonConfiguration.java
+++ service/src/main/java/org/transactions/configuration/JacksonConfiguration.java
@@ -13,7 +13,7 @@ public class JacksonConfiguration {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mapper.registerModule(new JavaTimeModule());
 
