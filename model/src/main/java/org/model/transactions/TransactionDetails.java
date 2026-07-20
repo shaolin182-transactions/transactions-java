9869c4fd0bb930165581109da21afc52d0854190
Julien Girard <girard.julien@gmail.com>
2026-07-20 22:40:40 +0200

    feat: Fix description validator

--- model/src/main/java/org/model/transactions/TransactionDetails.java
+++ model/src/main/java/org/model/transactions/TransactionDetails.java
@@ -31,7 +31,7 @@ public class TransactionDetails {
     private Float outcome;
 
     @Size(max = 512)
-    @Pattern(regexp = "^[\\p{L}0-9_@./# &,'-+:()]*$")
+    @Pattern(regexp = "^[\\p{L}0-9_@./# &,'\\-–$?+:()]*$")
     private String description;
 
     @Valid
